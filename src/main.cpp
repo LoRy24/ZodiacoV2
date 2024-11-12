0e5182f58315390f0ca97f4604d50340e5b58555
Lorenzo Rocca <51451299+LoRy24@users.noreply.github.com>
2024-11-12 16:34:03 +0100

    Completata la v2.0 del programma

--- src/main.cpp
+++ src/main.cpp
@@ -6,14 +6,17 @@
 // La UI sembra volutamente quella di un videogame arcade, per dare un tocco di originalità.
 //
 
+#pragma region Inclusioni
+
 // Inclusioni
 #include <iostream>
 #include <graphics.h>
 #include <unistd.h>
 #include <string>
+#include <cstdio>
+#include <time.h>
 
-// Namespace STD
-using namespace std;
+#pragma endregion
 
 #pragma region Definizioni
 
@@ -42,6 +45,7 @@ using namespace std;
 #define PULSANTI_MENU_DATA_NASCITA_IMAGE        "./img/pulsanti_menu_nascita.jpeg"
 #define SELEZIONE_DATA_GIORNO_IMAGE             "./img/data/selezione_data_giorno.jpeg"
 #define SELEZIONE_DATA_MESE_IMAGE               "./img/data/selezione_data_mese.jpeg"
+#define PREMI_Q_PER_USCIRE_IMAGE                "./img/premi_q_per_uscire.jpeg"
 
 #pragma endregion
 
@@ -55,7 +59,7 @@ int meseSelezionato = 1;
 int giorniPerMese[12] = {31, 29, 31, 30, 31, 30, 31, 31, 30, 31, 30, 31};
 
 // Nomi dei mesi
-string nomiMesi[12] = {
+std::string nomiMesi[12] = {
     "Gennaio",
     "Febbraio",
     "Marzo",
@@ -69,6 +73,34 @@ string nomiMesi[12] = {
     "Novembre",
     "Dicembre"
 };
+
+// Enum dei segni zodiacali
+enum ZodiacalSign {
+    CAPRICORNO,
+    ACQUARIO,
+    PESCI,
+    ARIETE,
+    TORO,
+    GEMELLI,
+    CANCRO,
+    LEONE,
+    VERGINE,
+    BILANCIA,
+    SCORPIONE,
+    SAGITTARIO
+};
+
+#pragma endregion
+
+#pragma region Funzioni speciali
+
+/**
+ * @brief Funzione per attendere un tot di millisecondi
+ * @param milliseconds Millisecondi da attendere
+ */
+void sleep_ms(int milliseconds) {
+    usleep(milliseconds * 1000);
+}
 
 #pragma endregion
 
@@ -85,145 +117,28 @@ int computeBasedOnNewWidth(int oldWidth, int oldHeight, int newWidth) {
     return (oldHeight * newWidth) / oldWidth;
 }
 
-// Ottiene i numeri da 1 a 31
+/**
+ * @brief Ottiene il nome del file per il numero
+ */
 char* getNumberFileName(int number, bool selected) {
-    // Nel non sia selezionato, applica quello non selezionato
-    if (!selected) {
-        switch (number) {
-            case 1:
-                return "./img/numbers/notSelected/1.jpeg";
-            case 2:
-                return "./img/numbers/notSelected/2.jpeg";  
-            case 3:
-                return "./img/numbers/notSelected/3.jpeg";
-            case 4:
-                return "./img/numbers/notSelected/4.jpeg";  
-            case 5:
-                return "./img/numbers/notSelected/5.jpeg";  
-            case 6:
-                return "./img/numbers/notSelected/6.jpeg";  
-            case 7:
-                return "./img/numbers/notSelected/7.jpeg";  
-            case 8:
-                return "./img/numbers/notSelected/8.jpeg";  
-            case 9:
-                return "./img/numbers/notSelected/9.jpeg";  
-            case 10:
-                return "./img/numbers/notSelected/10.jpeg";  
-            case 11:
-                return "./img/numbers/notSelected/11.jpeg";  
-            case 12:
-                return "./img/numbers/notSelected/12.jpeg";  
-            case 13:
-                return "./img/numbers/notSelected/13.jpeg";  
-            case 14:
-                return "./img/numbers/notSelected/14.jpeg";  
-            case 15:
-                return "./img/numbers/notSelected/15.jpeg";  
-            case 16:
-                return "./img/numbers/notSelected/16.jpeg";  
-            case 17:
-                return "./img/numbers/notSelected/17.jpeg";  
-            case 18:
-                return "./img/numbers/notSelected/18.jpeg";  
-            case 19:
-                return "./img/numbers/notSelected/19.jpeg";  
-            case 20:
-                return "./img/numbers/notSelected/20.jpeg";  
-            case 21:
-                return "./img/numbers/notSelected/21.jpeg";  
-            case 22:
-                return "./img/numbers/notSelected/22.jpeg";  
-            case 23:
-                return "./img/numbers/notSelected/23.jpeg";  
-            case 24:
-                return "./img/numbers/notSelected/24.jpeg";  
-            case 25:
-                return "./img/numbers/notSelected/25.jpeg";  
-            case 26:
-                return "./img/numbers/notSelected/26.jpeg";  
-            case 27:
-                return "./img/numbers/notSelected/27.jpeg";  
-            case 28:
-                return "./img/numbers/notSelected/28.jpeg";  
-            case 29:
-                return "./img/numbers/notSelected/29.jpeg";  
-            case 30:
-                return "./img/numbers/notSelected/30.jpeg";  
-            case 31:
-                return "./img/numbers/notSelected/31.jpeg";    
-            default:
-                return "\0";
-        }
-    }
-    else {
-        switch (number) {
-            case 1:
-                return "./img/numbers/selected/1.jpeg";
-            case 2:
-                return "./img/numbers/selected/2.jpeg";  
-            case 3:
-                return "./img/numbers/selected/3.jpeg";
-            case 4:
-                return "./img/numbers/selected/4.jpeg";  
-            case 5:
-                return "./img/numbers/selected/5.jpeg";  
-            case 6:
-                return "./img/numbers/selected/6.jpeg";  
-            case 7:
-                return "./img/numbers/selected/7.jpeg";  
-            case 8:
-                return "./img/numbers/selected/8.jpeg";  
-            case 9:
-                return "./img/numbers/selected/9.jpeg";  
-            case 10:
-                return "./img/numbers/selected/10.jpeg";  
-            case 11:
-                return "./img/numbers/selected/11.jpeg";  
-            case 12:
-                return "./img/numbers/selected/12.jpeg";  
-            case 13:
-                return "./img/numbers/selected/13.jpeg";  
-            case 14:
-                return "./img/numbers/selected/14.jpeg";  
-            case 15:
-                return "./img/numbers/selected/15.jpeg";  
-            case 16:
-                return "./img/numbers/selected/16.jpeg";  
-            case 17:
-                return "./img/numbers/selected/17.jpeg";  
-            case 18:
-                return "./img/numbers/selected/18.jpeg";  
-            case 19:
-                return "./img/numbers/selected/19.jpeg";  
-            case 20:
-                return "./img/numbers/selected/20.jpeg";  
-            case 21:
-                return "./img/numbers/selected/21.jpeg";  
-            case 22:
-                return "./img/numbers/selected/22.jpeg";  
-            case 23:
-                return "./img/numbers/selected/23.jpeg";  
-            case 24:
-                return "./img/numbers/selected/24.jpeg";  
-            case 25:
-                return "./img/numbers/selected/25.jpeg";  
-            case 26:
-                return "./img/numbers/selected/26.jpeg";  
-            case 27:
-                return "./img/numbers/selected/27.jpeg";  
-            case 28:
-                return "./img/numbers/selected/28.jpeg";  
-            case 29:
-                return "./img/numbers/selected/29.jpeg";  
-            case 30:
-                return "./img/numbers/selected/30.jpeg";  
-            case 31:
-                return "./img/numbers/selected/31.jpeg";    
-            default:
-                return "\0";
-        }
-    }
+    // Creiamo il buffer
+    static char buffer[100] = {0};
+    sprintf(buffer, "./img/numbers/%s/%d.jpeg", selected ? "selected" : "notSelected", number);
+
+    // Ritorna il buffer
+    return buffer;
+}
+
+/**
+ * @brief Ottiene il nome del file per il segno zodiacale
+ */
+char* getSignFileName(int sign) {
+    // Creiamo il buffer
+    static char buffer[100] = {0};
+    sprintf(buffer, "./img/sign/%d.jpeg", sign);
+
+    // Ritorna il buffer
+    return buffer;
 }
 
 #pragma endregion
@@ -626,16 +541,188 @@ BirthDate requestBirthDateForm() {
 
 #pragma endregion
 
+#pragma region Calcolo Segno Zodiacale
+
+// Costanti per i segni zodiacali
+ZodiacalSign segniZodiacaliPrimaMeta[12] = {
+    CAPRICORNO, ACQUARIO, PESCI, ARIETE, TORO, GEMELLI, CANCRO, LEONE, VERGINE, BILANCIA, SCORPIONE, SAGITTARIO
+};
+
+ZodiacalSign segniZodiacaliSecondaMeta[12] = {
+    ACQUARIO, PESCI, ARIETE, TORO, GEMELLI, CANCRO, LEONE, VERGINE, BILANCIA, SCORPIONE, SAGITTARIO, CAPRICORNO
+};
+
+/**
+ * @brief Ottieni il segno zodiacale in base alla data di nascita. Considera del segno della prima metà fino al giorno 
+ *        di scambio. Quindi, se ci si trova in condizione di cuspide, bisogna considerare il segno della seconda metà \
+ *        per l'unione. Es: 19 Gennaio, Capricorno-Acquario (0-1), dove 0 è Capricorno e 1 è Acquario. La funzione ritornerà 
+ *        0 quindi bisogna prendere 0 + 1.
+ */
+ZodiacalSign getZodiacalSign(BirthDate birthDate, bool* cuspide) {
+    // Definisco il limite e il giorno di scambio
+    int limit = 0, changeDay = 0;
+
+    // Definisco il mese
+    int month = birthDate.month + 1;
+
+    // In base al mese, imposto il limite di giorni
+    switch (month) {
+        case 4:
+        case 6:
+        case 9:
+        case 11: {
+            limit = 30;
+            break;
+        }
+
+        case 2: {
+            limit = 29;
+            break;
+        }
+
+        default: {
+            limit = 31;
+            break;
+        }
+    }
+    
+    // Sempre in base al mese, imposto il giorno di scambio
+    switch (month) {
+        case 4:
+        case 1:
+        case 2: {
+            changeDay = 19;
+            break;
+        }
+
+        case 3:
+        case 5:
+        case 6: {
+            changeDay = 20;
+            break;
+        }
+
+        case 11:
+        case 12: {
+            changeDay = 21;
+            break;
+        }
+
+        case 8: {
+            changeDay = 23;
+            break;
+        }
+
+        default: {
+            changeDay = 22;
+            break;
+        }
+    }
+
+    // Verifica se è in cuspide
+    *cuspide = birthDate.day == changeDay || birthDate.day == changeDay + 1;
+
+    // Ritorno il segno
+    return birthDate.day <= changeDay ? segniZodiacaliPrimaMeta[birthDate.month] : segniZodiacaliSecondaMeta[birthDate.month];
+}
+
+#pragma endregion
+
 #pragma region Funzioni Grafiche - Menu Segno Zodiacale
 
+/**
+ * @brief Avvia l'animazione per il segno zodiacale
+ */
+void playZodiacalSignAnimation(ZodiacalSign zodiacalSign) {
+    // Definisco le schermate da cambiare
+    int frames = 24;
 
+    // Il programma inizierà a rallentare 6 schermate prima, dunque dobbiamo calcoalre il segno a quanto dista.
+    // Esempio: se il segno è 0, non aggiungiamo nulla a 18, se è 6, aggiungiamo 6 a 18, se è 12, aggiungiamo 12 a 18.
+    frames += zodiacalSign;
+
+    // Dimensioni immagine schermata
+    int initialWidth = 1300;
+    int initialHeight = 800;
+
+    // Va piazzata al centro totale dello schermo, dunque:
+    int x = (WIDTH - initialWidth) / 2;
+    int y = (HEIGHT - initialHeight) / 2;
+
+    // L'immagine non va ridimensionata, quindi procediamo con l'animazione
+    int frame = 0;
+    for (frame = 0; frame < frames; frame++) {
+        // Stampa l'immagine
+        readimagefile(getSignFileName(frame % 12), x, y, x + initialWidth, y + initialHeight);
+
+        // Debug
+        std::cout << "Frame attuale: " << frame << std::endl;
+
+        // Attendi il tempo dell'animazione (500ms)
+        usleep(500 * 100);
+    }
+
+    // Debug
+    std::cout << "Stampa ultimi 12 frame: " << std::endl;
+
+    // Definiamo i tempi di attesa per i frame
+    int framesSleepMs[12] = {75, 100, 125, 150, 200, 250, 300, 400, 500, 650, 850, 1000};
+
+    // Stampa gli ultimi 12 frame
+    for (int fA = 0; frame < frames + 12; frame++, fA++) {
+        // Stampa l'immagine
+        readimagefile(getSignFileName(frame % 12), x, y, x + initialWidth, y + initialHeight);
+
+        // Debug
+        std::cout << "Frame attuale: " << frame << std::endl;
+
+        // Attendi il tempo dell'animazione che cresce
+        struct timespec ts;
+        ts.tv_nsec = 1000 * 1000 * (framesSleepMs[fA] - ((framesSleepMs[fA] / 1000) * 1000));
+        ts.tv_sec = framesSleepMs[fA] / 1000;
+        nanosleep(&ts, NULL);
+    }
+
+    // Aspetta 2 secondi
+    sleep(2);
+}
+
+/**
+ * @brief Stampa la schermata del segno zodiacale
+ */
+void showZodiacalSign(BirthDate birthDate) {
+    // Calcola il segno zodiacale
+    bool cuspide = false;
+    ZodiacalSign zodiacalSign = getZodiacalSign(birthDate, &cuspide);
+
+    // Debug
+    std::cout << "[DEBUG] Segno Zodiacale: " << zodiacalSign << std::endl;
+    std::cout << "[DEBUG] Configurazione schermata segno zodiacale" << std::endl;
+
+    // Avvia l'animazione
+    playZodiacalSignAnimation(zodiacalSign);
+
+    // Scrivi il messaggio finale per uscire
+    int width = 200, height = computeBasedOnNewWidth(1200, 240, width);
+    readimagefile(PREMI_Q_PER_USCIRE_IMAGE, 291, 453, 291 + width, 453 + height);
+
+    // Attendi un input
+    while (true) {
+        // Attendi per un carattere da inserire
+        char input = getch();
+
+        // Se viene premuto q, esci
+        if (input == 'Q' || input == 'q') {
+            break;
+        }
+    }
+}
 
 #pragma endregion
 
 #pragma region Routine Programma
 
 // Main
-
 int main() {
     // Inizializza lo schermo
     setupScreen();
@@ -664,8 +751,14 @@ int main() {
     // Pulisci lo schermo
     cleardevice();
 
-    // Attendi un input
-    getch();
+    // Debug
+    std::cout << "[DEBUG] Schermo pulito, avvio schermata segno zodiacale" << std::endl;
+
+    // Stampa la schermata del segno zodiacale
+    showZodiacalSign(birthDate);
+
+    // Debug
+    std::cout << "[DEBUG] Schermata segno zodiacale completata, attesa pulsante di ritorno (esc)" << std::endl;
 
     // Chiudi lo schermo
     closegraph();
